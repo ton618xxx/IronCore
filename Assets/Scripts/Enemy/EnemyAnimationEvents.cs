503f059a86a8638f7e77d5b48c6a85d2b12e033a
Dmitry Nazarov <roygbiv1969@yandex.ru>
2025-12-01 18:20:45 +0300

    dead state

--- Assets/Scripts/Enemy/EnemyAnimationEvents.cs
+++ Assets/Scripts/Enemy/EnemyAnimationEvents.cs
@@ -2,16 +2,7 @@ using UnityEngine;
 
 public class EnemyAnimationEvents : MonoBehaviour
 {
-    private Enemy enemy;
 
-    private void Awake()
-    {
-        enemy = GetComponentInParent<Enemy>();
-    }
-
-    public void AnimationTrigger() => enemy.AnimationTrigger();
-
-    public void StartManualMovement() => enemy.ActivateManualMovement(true);
     public void StopManualMovement() => enemy.ActivateManualMovement(false);
 
     public void StartManualRotation() => enemy.ActivateManualRotation(true); 
--- Assets/Scripts/Enemy/MoveState_Melee.cs
+++ Assets/Scripts/Enemy/MoveState_Melee.cs
@@ -12,21 +12,7 @@ public class MoveState_Melee : EnemyState
     
     public MoveState_Melee(Enemy enemyBase, EnemyStateMachine stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
     {
-      enemy = enemyBase as Enemy_Melee;  
-    }
 
-    public override void Enter()
-    {
-        base.Enter();
-
-        enemy.agent.speed = enemy.moveSpeed; 
-
-        destination = enemy.GetPatrolDestination();
-
-        enemy.agent.SetDestination(destination);
-    }
-
-    public override void Exit()
     {
         base.Exit(); 
     }
@@ -37,21 +23,7 @@ public class MoveState_Melee : EnemyState
 
         if (enemy.PlayerInAggressionRange())
         {
-            stateMachine.ChangeState(enemy.recoveryState);
-            return;
-        }
-        
-        enemy.transform.rotation = enemy.FaceTarget(GetNextPathPoint());
-        
-        if (enemy.agent.remainingDistance <= enemy.agent.stoppingDistance + .05f)
-            stateMachine.ChangeState(enemy.idleState); 
-    }
-}
-
-
-
-       
-         
+            stateMachine.Cha
         
             
         
